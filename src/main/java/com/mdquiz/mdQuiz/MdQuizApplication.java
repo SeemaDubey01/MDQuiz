178f47dbc7834e58157a5a86f2425a3a7442f81c
Manish Dubey <manish.dubey01@ad.infosys.com>
2020-05-15 21:02:05 +0200

    pom2

--- src/main/java/com/mdquiz/mdQuiz/MdQuizApplication.java
+++ src/main/java/com/mdquiz/mdQuiz/MdQuizApplication.java
@@ -2,11 +2,32 @@ package com.mdquiz.mdQuiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class MdQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdQuizApplication.class, args);
 	}
+	@RequestMapping("/")
+	public String index() {
+	System.out.println("inside index");
+	return "index";
+	}
+	@RequestMapping("/page2")
+	public String page2() {
+	return "page2";
+	}
+	@RequestMapping("/StartQuiz")
+	public String startpage() {
+	return "startquiz";
+	}
+	@RequestMapping("/dbpage")
+	public String dbpage() {
+	return "dbpage";
+	}
+	
 }
--- src/main/java/com/mdquiz/mdQuiz/MdQuizController.java
+++ src/main/java/com/mdquiz/mdQuiz/MdQuizController.java
@@ -7,23 +7,7 @@ import java.sql.*;
 @Controller
 public class MdQuizController {
 
-	@RequestMapping("/")
-	public String index() {
-	return "index";
-	}
-	@RequestMapping("/page2")
-	public String page2() {
-	return "page2";
-	}
-	@RequestMapping("/StartQuiz")
-	public String startpage() {
-	return "startquiz";
-	}
-	@RequestMapping("/dbpage")
-	public String dbpage() {
-	return "dbpage";
-	}
-	
+
 	/*public Connection getConnectionToDB(){
 		try {
 			Connection conn = DriverManager.getConnection("", "", "");
