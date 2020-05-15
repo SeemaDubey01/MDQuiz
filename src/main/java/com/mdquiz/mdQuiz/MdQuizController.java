420d4e59ac1f9fbbb0428c7b7378e3e0263df3db
Manish Dubey <manish.dubey01@ad.infosys.com>
2020-05-15 20:42:10 +0200

    pom

--- src/main/java/com/mdquiz/mdQuiz/MdQuizController.java
+++ src/main/java/com/mdquiz/mdQuiz/MdQuizController.java
@@ -2,6 +2,7 @@ package com.mdquiz.mdQuiz;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.sql.*;
 
 @Controller
 public class MdQuizController {
@@ -22,5 +23,14 @@ public class MdQuizController {
 	public String dbpage() {
 	return "dbpage";
 	}
+	
+	/*public Connection getConnectionToDB(){
+		try {
+			Connection conn = DriverManager.getConnection("", "", "");
+			return conn;
+		} catch (Exception e) {
+			return null;
+		}
+	}*/
 
 }
